afd324c92bf62b2fbcd8191c5b810808c6139e23
Izzy <isabelle_thorpe@outlook.com>
2023-04-10 12:01:43 +0100

    test update

---
+++ src/api/employee-characteristic/controllers/employee-characteristic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * employee-characteristic controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::employee-characteristic.employee-characteristic');
---
+++ src/api/employee-characteristic/routes/employee-characteristic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * employee-characteristic router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::employee-characteristic.employee-characteristic');
