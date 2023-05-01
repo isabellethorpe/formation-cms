63aef79e2408abf978e1881502693f4af27a00e9
Izzy <isabelle_thorpe@outlook.com>
2023-05-01 10:27:04 +0100

    removed attempted login auth

--- src/extensions/users-permissions/controllers/user.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  async checkUser(ctx) {
-    const { email } = ctx.params;
-    const user = await strapi.plugins["users-permissions"].services.user.fetch({
-      email,
-    });
-    return !!user;
-  },
-};
